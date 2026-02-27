94c4b13617039a58dc1c4251f736992dff533b7b
Arslan Ovezov <arslan.datascience@gmail.com>
2026-02-27 11:54:24 +0500

    ars3

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -44,15 +44,15 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "real_estate.middlewares.RealEstateSpiderMiddleware": 543,
-#}
+SPIDER_MIDDLEWARES = {
+   "real_estate.middlewares.RealEstateSpiderMiddleware": 543,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "real_estate.middlewares.RealEstateDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   "real_estate.middlewares.RealEstateDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "real_estate.pipelines.RealEstatePipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "real_estate.pipelines.RealEstatePipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- real_estate/real_estate/spiders/real_first.py
+++ real_estate/real_estate/spiders/real_first.py
@@ -1,5 +1,5 @@
 import scrapy
-import requests
+
 
 
 class RealFirstSpider(scrapy.Spider):
