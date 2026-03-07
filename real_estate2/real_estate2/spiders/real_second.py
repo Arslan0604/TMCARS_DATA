8ac7dc3892b2e32889e423282aa4b8be127c65cb
Arslan Ovezov <arslan.datascience@gmail.com>
2026-03-07 15:35:46 +0500

    ars6

--- real_estate2/real_estate2/spiders/real_second.py
+++ real_estate2/real_estate2/spiders/real_second.py
@@ -14,22 +14,31 @@ class RealSecondSpider(scrapy.Spider):
         yield scrapy.Request(
             urls,
             callback=self.parse,
-            meta={"offset": offset},
+            meta={"offset": offset, "playwright": True },
         )                                 # extracting data from the first page
 
     def parse(self, response):
         reals = response.css('.item-card2-desc')
         
         for real in reals:
-            yield {
-                'title': real.css('.font-weight-bold::text').get(),
-                'description': real.css('.max-lines-p-desc::text').get(),
-                'time_to_paste': real.css('.mt-2::text').get(),
-                'location': real.css('.ms-3::text').get(),
-                'price': real.css('.h5::text').get(),
-                'link': real.css('span a').attrib['href'],
+            detail_url = response.urljoin(real.css("span a::attr(href)").get())
+            meta_data = {
+                "title": real.css('.font-weight-bold::text').get(),
+                "price": real.css('.h5::text').get(),
             }
-            
+            yield scrapy.Request(
+                detail_url,
+                callback=self.parse_detail,
+                meta={
+                    "playwright": True,
+                    "playwright_page_methods": [
+                        PageMethod("click", "button.show-phone"),
+                        PageMethod("wait_for_selector", "a[href^='tel']")  # надо найти селектор телефона по поставить здесь
+                    ],
+                    **meta_data
+                }
+            )
+                
         # pagination
         offset = response.meta["offset"] + 150
         next_url = f"{self.start_urls[0]}?offset={offset}&max=150&lang=ru"
@@ -39,5 +48,36 @@ class RealSecondSpider(scrapy.Spider):
             yield scrapy.Request(
                 next_url,
                 callback=self.parse,
-                meta={"offset": offset},
+                meta={"offset": offset, "playwright": True }
             )
+            
+            
+    def parse_detail(self, response):
+        phone = response.css("a[href^='tel']::text").get()  # замените на правильный селектор для телефона
+        
+        rows = response.css("tbody tr")
+        
+        data = {}
+        
+        for row in rows:
+            key = row.css("span::text").get()
+            value = row.css("td::text").get()
+            
+            if key:
+                key = key.replace(" :", "").strip()
+                data[key] = value
+        
+        yield {
+           "title": response.meta["title"],
+           "price": response.meta["price"],
+           "link": response.url,
+           "phone": phone,
+           "details": data 
+            }
+                           
+                
+                
+            
+        #  response.css(".entry-content-text p::text").get() # selector description
+        # response.css("td:has(span.font-weight-bold)::text").get() # selector for category 
+        
