1c4131c4fa00adb1b5b866cacffaf02907adc9cf
Arslan Ovezov <arslan.datascience@gmail.com>
2026-03-07 11:50:05 +0500

    ars3

--- real_estate/real_estate/spiders/real_first.py
+++ real_estate/real_estate/spiders/real_first.py
@@ -6,7 +6,16 @@ class RealFirstSpider(scrapy.Spider):
     allowed_domains = ["tmcars.info"]
     start_urls = ["https://tmcars.info/others/nedvijimost/prodaja-kvartir-i-domov"]
 
-  
+    def start_requests(self):             # extracting data from the first page 
+        offset = 0
+        
+        urls = f"{self.start_urls[0]}?offset={offset}&max=150&lang=ru"
+        
+        yield scrapy.Request(
+            urls,
+            callback=self.parse,
+            meta={"offset": offset},
+        )                                 # extracting data from the first page
 
     def parse(self, response):
         reals = response.css('.item-card2-desc')
@@ -20,7 +29,18 @@ class RealFirstSpider(scrapy.Spider):
                 'price': real.css('.h5::text').get(),
                 'link': real.css('span a').attrib['href'],
             }
-
+            
+        # pagination
+        offset = response.meta["offset"] + 150
+        next_url = f"{self.start_urls[0]}?offset={offset}&max=150&lang=ru"
+        
+        #stop if no items returned
+        if reals:
+            yield scrapy.Request(
+                next_url,
+                callback=self.parse,
+                meta={"offset": offset},
+            )
 
 # real.css('.h5::text').get() - для получения price из элемента с классом h5
 # real.css('.max-lines-p-desc::text').get() - для получения description из элемента с классом max-lines-p-desc
