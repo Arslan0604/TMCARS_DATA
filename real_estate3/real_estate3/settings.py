9daa9bcb9bfc8418d950e3db0c5a1d7f563c22ea
Arslan Ovezov <arslan.datascience@gmail.com>
2026-06-02 18:58:20 +0500

    ars1

--- real_estate3/real_estate3/settings.py
+++ real_estate3/real_estate3/settings.py
@@ -24,7 +24,7 @@ CLOSESPIDER_ITEMCOUNT = 5
 # Concurrency and throttling settings
 CONCURRENT_REQUESTS = 1
 CONCURRENT_REQUESTS_PER_DOMAIN = 3
-DOWNLOAD_DELAY = 5
+DOWNLOAD_DELAY = 4
 #USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36"
 RANDOMIZE_DOWNLOAD_DELAY = True
 
