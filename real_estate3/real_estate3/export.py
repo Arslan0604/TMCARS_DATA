530069d83095f9c5c57983170f07c9b28868d307
Arslan Ovezov <arslan.datascience@gmail.com>
2026-06-14 14:14:20 +0500

    ars1

--- real_estate3/real_estate3/export.py
+++ real_estate3/real_estate3/export.py
@@ -1,3 +1,30 @@
+# import pandas as pd
+# import psycopg2
+
+# conn = psycopg2.connect(
+#     database="parser_db",
+#     user="macbookpro",
+#     host="localhost",
+#     port=5432
+# )
+
+# df = pd.read_sql("SELECT * FROM real_estate3", conn)
+
+# # очистка
+# df = df.drop_duplicates(subset=["link"])
+
+# # фильтр (пример)
+# df = df[df["location"].notna()]
+
+# # сортировка
+# df = df.sort_values(by="id", ascending=False)
+
+# # экспорт
+# df.to_excel("real_estate.xlsx", index=False)
+
+# print("DONE")
+
+
 import pandas as pd
 import psycopg2
 
@@ -8,18 +35,31 @@ conn = psycopg2.connect(
     port=5432
 )
 
-df = pd.read_sql("SELECT * FROM real_estate3", conn)
+query = """
+SELECT *
+FROM real_estate3
+WHERE phone IN (
+    SELECT phone
+    FROM real_estate3
+    WHERE phone IS NOT NULL
+      AND phone <> ''
+    GROUP BY phone
+    HAVING COUNT(*) = 1
+)
+"""
 
-# очистка
+df = pd.read_sql(query, conn)
+
+# убрать дубли по ссылке (на всякий случай)
 df = df.drop_duplicates(subset=["link"])
 
-# фильтр (пример)
+# убрать записи без города
 df = df[df["location"].notna()]
 
-# сортировка
+# последние объявления сверху
 df = df.sort_values(by="id", ascending=False)
 
 # экспорт
-df.to_excel("real_estate.xlsx", index=False)
+df.to_excel("private_owners.xlsx", index=False)
 
-print("DONE")
+print(f"Exported {len(df)} private owners")
